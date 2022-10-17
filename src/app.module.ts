c8b98df46b816403f6b99f7bca356f98961860d1
fharavena <fharavena@noresponse.com>
2022-10-17 16:35:48 -0300

    auth create User unique username

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './tasks/task.entity';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TasksController } from './tasks.controller';
-import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
